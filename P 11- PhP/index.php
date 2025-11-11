59f8667453ba35373ccb2d285d71e7e859a6bedd
Max12445 <maximusmori19>
2025-11-11 17:16:43 -0300

    star php

---
+++ P 11- PhP/index.php
@@ -0,0 +1,11 @@
+<h1>Helo Word</h1>
+<?php
+
+$x = 10;   //declaração de varialvel
+// ele vai aceitar a variavel independente do valor dela
+$helloWord = "Hello World em PHP";
+$true = true;
+
+echo($helloWord)
+
+?>
